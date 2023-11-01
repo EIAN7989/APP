49a2a82f2fe7cfddb9b6c90e951bdb3d275fe278
EIAN7989 <124330576+EIAN7989@users.noreply.github.com>
2023-11-01 18:55:09 +0800

    Add files via upload

---
+++ iOS  提醒事项 to 日历/README.md
@@ -0,0 +1,20 @@
+# Sync_reminder_to_calendar
+用于同步iOS提醒事项的内容到日历中
+## 脚本功能介绍
+
+- 用于同步iOS提醒事项的内容到日历中
+
+## 使用方法
+
+- iOS上下载Scriptable软件
+
+- 打开Scriptable添加脚本
+
+- 在快捷指令中自行设置自动化,打开提醒事项和日历其中任意一个应用时运行Scriptable脚本的自动化指令
+
+## 推荐使用脚本3
+
+- 视频地址：https://www.bilibili.com/video/BV1dK4y1d7xT/?t=16.4&vd_source=03f506a120c59a401554cb1104c9ee31
+- https://www.dodolo.top/pages/summary/scriptable.html
+- https://github.com/zackertypical/ios_script
+- 原作者发布地址：https://www.bilibili.com/video/BV1pK4y1Y7DX/?spm_id_from=333.788.video.desc.click&vd_source=03f506a120c59a401554cb1104c9ee31
---
+++ iOS  提醒事项 to 日历/sync_reminder_to_calendar_1.js
@@ -0,0 +1,141 @@
+//要同步的时间段，当前时间前后n个月，例如，当前为5月，设置为2时，同步时间段为3～7月
+var dur_month = 2;
+
+const startDate = new Date();
+startDate.setMonth(startDate.getMonth() - dur_month);
+console.log(`日历的开始时间 ${startDate.toLocaleDateString()}`);
+
+const endDate = new Date();
+endDate.setMonth(endDate.getMonth() + dur_month);
+console.log(`日历的结束时间 ${endDate.toLocaleDateString()}`);
+
+const reminders = await Reminder.allDueBetween(startDate, endDate);
+console.log(`获取 ${reminders.length} 条提醒事项`);
+
+var calendar = await Calendar.forEvents();
+
+//获取日历名和对应的日历
+var m_dict = {};
+for (cal of calendar) {
+	m_dict[cal.title] = cal;
+	//console.log(`日历:${cal.title}`)
+}
+
+const events = await CalendarEvent.between(startDate, endDate, calendar);
+console.log(`获取 ${events.length} 条日历`);
+console.log(events);
+
+for (const reminder of reminders) {
+	reminder.notes =
+		!reminder.notes || reminder.notes == null || reminder.notes == "undefined"
+			? "无"
+			: reminder.notes;
+	//reminder的标识符
+	const targetNote = `[Reminder ID] ${reminder.identifier}`;
+	const [targetEvent] = events.filter(
+		(e) => e.notes != null && e.notes.indexOf(targetNote) != -1
+	); //过滤重复的reminder
+	if (!m_dict[reminder.calendar.title]) {
+		console.warn("找不到日历" + reminder.calendar.title);
+		continue;
+	}
+	if (targetEvent) {
+		//console.log(`找到已经创建的事项 ${reminder.title}`)
+		updateEvent(targetEvent, reminder);
+	} else {
+		console.warn(`创建事项 ${reminder.title} 到 ${reminder.calendar.title}`);
+		const newEvent = new CalendarEvent();
+		newEvent.notes = reminder.notes + "\n\n" + targetNote; //要加入备注
+		updateEvent(newEvent, reminder);
+	}
+}
+
+Script.complete();
+
+//设置period
+function setPeriod(event, period, description) {
+	const supplement =
+		description == "延期" || description == "提前" ? "完成" : "";
+	if (period < 3600) {
+		return (subHeading =
+			Math.floor((period / 60).toFixed(1)) == 0
+				? `准时完成`
+				: `${description}${(period / 60).toFixed()}分钟${supplement}`);
+	} else if (period >= 3600 && period <= 3600 * 24) {
+		return (subHeading =
+			((period % 3600) / 60).toFixed() == 0
+				? `${description}${(period / 3600).toFixed()}小时${supplement}`
+				: `${description}${Math.floor((period / 3600).toFixed(2))}小时${(
+						(period % 3600) /
+						60
+				  ).toFixed()}分钟${supplement}`);
+	} else {
+		return (subHeading =
+			((period % (3600 * 24)) / 3600).toFixed() == 0
+				? `${description}${(period / 3600 / 24).toFixed()}天${supplement}`
+				: `${description}${(period / 3600 / 24).toFixed()}天${(
+						(period % (3600 * 24)) /
+						3600
+				  ).toFixed()}小时${supplement}`);
+	}
+}
+
+//日历中创建提醒
+function updateEvent(event, reminder) {
+	cal_name = reminder.calendar.title;
+	cal = m_dict[cal_name];
+	event.calendar = cal;
+	// console.warn(event.calendar.title)
+	// 已完成事项
+	if (reminder.isCompleted) {
+		event.isAllDay = false;
+		event.startDate = reminder.dueDate;
+		event.endDate = reminder.completionDate;
+		var period = (reminder.dueDate - reminder.completionDate) / 1000;
+		period = period.toFixed();
+		if (period < 0) {
+			period = -period;
+			let titleTail = setPeriod(event, period, "延期");
+			event.title = `✅${reminder.title} (${titleTail})`;
+		} else if (period == 0) {
+			event.title = `✅${reminder.title} (准时完成)`;
+		} else {
+			let titleTail = setPeriod(event, period, "提前");
+			event.title = `✅${reminder.title} (${titleTail})`;
+			event.endDate = reminder.dueDate;
+			event.startDate = reminder.completionDate;
+		}
+	}
+	// 未完成事项
+	else {
+		const nowtime = new Date();
+		var period = (reminder.dueDate - nowtime) / 1000;
+		period = period.toFixed();
+		if (period < 0) {
+			// 待办顺延
+			period = -period;
+			let titleTail = setPeriod(event, period, "已延期");
+			// 如果不是在同一天,设置为全天事项
+			if (reminder.dueDate.getDate() != nowtime.getDate()) {
+				event.title = `❌${reminder.title} (${titleTail})`;
+				event.startDate = nowtime;
+				event.endDate = nowtime;
+				event.isAllDay = true;
+			}
+			// 在同一天的保持原来的时间
+			else {
+				event.title = `⭕️${reminder.title} (${titleTail})`;
+				event.isAllDay = false;
+				event.startDate = reminder.dueDate;
+				event.endDate = nowtime;
+			}
+		} else {
+			event.isAllDay = false;
+			let titleTail = setPeriod(event, period, "还剩");
+			event.title = `⭕️${reminder.title} (${titleTail})`;
+			event.startDate = reminder.dueDate;
+			event.endDate = reminder.dueDate;
+		}
+	}
+	event.save();
+}
